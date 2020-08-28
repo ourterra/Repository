80d61f89fd169c356420cd68e7544f09bbd43938
ourterra <68116146+ourterra@users.noreply.github.com>
2020-08-28 22:25:55 +0200

    Update Task.stories.js

--- src/components/Task.stories.js
+++ src/components/Task.stories.js
@@ -1,38 +1,52 @@
-﻿// src/components/Task.stories.js
+// src/components/TaskList.stories.js
 
 import React from 'react';
 
-import Task from './Task';
+import TaskList from './TaskList';
+import * as TaskStories from './Task.stories';
 
 export default {
-    component: Task,
-    title: 'Task',
+  component: TaskList,
+  title: 'TaskList',
+  decorators: [story => <div style={{ padding: '3rem' }}>{story()}</div>],
 };
 
-const Template = args => <Task {...args} />;
+const Template = args => <TaskList {...args} />;
 
 export const Default = Template.bind({});
 Default.args = {
-    task: {
-        id: '1',
-        title: 'Test Task',
-        state: 'TASK_INBOX',
-        updatedAt: new Date(2018, 0, 1, 9, 0),
-    },
+  // Shaping the stories through args composition.
+  // The data was inherited from the Default story in task.stories.js.
+  tasks: [
+    { ...TaskStories.default.args, id: '1', title: 'Task 1' },
+    { ...TaskStories.default.args, id: '2', title: 'Task 2' },
+    { ...TaskStories.default.args, id: '3', title: 'Task 3' },
+    { ...TaskStories.default.args, id: '4', title: 'Task 4' },
+    { ...TaskStories.default.args, id: '5', title: 'Task 5' },
+    { ...TaskStories.default.args, id: '6', title: 'Task 6' },
+  ],
 };
 
-export const Pinned = Template.bind({});
-Pinned.args = {
-    task: {
-        ...Default.args.task,
-        state: 'TASK_PINNED',
-    },
+export const WithPinnedTasks = Template.bind({});
+WithPinnedTasks.args = {
+  // Shaping the stories through args composition.
+  // Inherited data coming from the Default story.
+  tasks: [
+    ...Default.args.tasks.slice(0, 5),
+    { id: '6', title: 'Task 6 (pinned)', state: 'TASK_PINNED' },
+  ],
 };
 
-export const Archived = Template.bind({});
-Archived.args = {
-    task: {
-        ...Default.args.task,
-        state: 'TASK_ARCHIVED',
-    },
+export const Loading = Template.bind({});
+Loading.args = {
+  tasks: [],
+  loading: true,
+};
+
+export const Empty = Template.bind({});
+Empty.args = {
+  // Shaping the stories through args composition.
+  // Inherited data coming from the Loading story.
+  ...Loading.args,
+  loading: false,
 };
