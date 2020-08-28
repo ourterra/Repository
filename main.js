5333778842404061b5e477566cdc7584d852f916
ourterra <68116146+ourterra@users.noreply.github.com>
2020-08-28 22:56:58 +0200

    Update main.js

--- main.js
+++ main.js
@@ -13,4 +13,12 @@ module.exports = {
       }
     },
   ],
+  module.exports = {
+  stories: ['../src/components/**/*.stories.js'],
+  addons: [
+    '@storybook/addon-links',
+    '@storybook/addon-essentials',
+    '@storybook/preset-create-react-app',
+  ],
+};
 };
