5cd70ae4b2fed7c6f2f170faafb6c7da19f83988
ourterra <68116146+ourterra@users.noreply.github.com>
2020-08-28 20:54:11 +0200

    Update main.js

--- main.js
+++ main.js
@@ -1,3 +1,16 @@
 module.exports = {
   addons: ['@storybook/addon-controls'],
 };
+module.exports = {
+  addons: [
+    {
+      name: '@storybook/preset-scss',
+      options: {
+        cssLoaderOptions: {
+           modules: true,
+           localIdentName: '[name]__[local]--[hash:base64:5]',
+        }
+      }
+    },
+  ],
+};
