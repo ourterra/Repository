1ee7128d76b2e83c9a055c73f3907dab48e26ff1
ourterra <68116146+ourterra@users.noreply.github.com>
2020-08-28 23:06:09 +0200

    Update preview.js

--- .storybook/preview.js
+++ .storybook/preview.js
@@ -1,11 +1,4 @@
-//export const parameters = {
-  //layout: 'centered',
-  //actions: { argTypesRegex: "^on[A-Z].*" },
-//}
-
-import '../src/index.css';
-
-// Configures Storybook to log the actions(onArchiveTask and onPinTask) in the UI.
 export const parameters = {
-  actions: { argTypesRegex: '^on[A-Z].*' },
-};
+  layout: 'centered',
+  actions: { argTypesRegex: "^on[A-Z].*" },
+}
