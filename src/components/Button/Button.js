5733374c206427c16d22598e5b27686b0f83f1ed
ourterra <68116146+ourterra@users.noreply.github.com>
2020-08-29 12:13:36 +0200

    Update Button.js

--- src/components/Button/Button.js
+++ src/components/Button/Button.js
@@ -1,7 +1,7 @@
 // Inside src/components/Button/Button.js
 
 import React from 'react';
-
+import './Button.css';
 const Button = ({ children, ...rest }) => {
   return (
     <button className="button" {...rest}>
