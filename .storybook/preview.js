46e386e786ec0d0887101b925b29bcb43b3caa92
ourterra <68116146+ourterra@users.noreply.github.com>
2020-09-13 15:24:20 +0200

    addCompo

--- .storybook/preview.js
+++ .storybook/preview.js
@@ -1,4 +1,13 @@
-export const parameters = {
-  layout: 'centered',
-  actions: { argTypesRegex: "^on[A-Z].*" },
-}
+
+export const globalTypes = {
+  theme: {
+    name: 'Theme',
+    description: 'Global theme for components',
+    defaultValue: 'light',
+    toolbar: {
+      icon: 'circlehollow',
+      // array of plain string values or MenuItem shape (see below)
+      items: ['light', 'dark'],
+    },
+  },
+};
